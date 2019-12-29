77c78f240c54f17ffc55145b86d9b851fc49f610
aamirali <sufaamir@gmail.com>
2019-12-29 13:58:36 -0500

    add new file

---
+++ 4IR.js
@@ -0,0 +1,12 @@
+Zia Khan
+
+In our innovation class we will be using this tool
+for Agile Teams: https: //www.atlassian.com/software/jira
+
+    Also check these out:
+    https: //www.atlassian.com/agile
+    https: //www.atlassian.com/agile/tutorials/sprints
+    https: //www.scrum.org/resources/blog/five-questions-sprint-goal
+    https: //scrumorg-website-prod.s3.amazonaws.com/drupal/2019-09/2019-09-Kanban-Guide-for-Scrum-Teams-English.pdf?nexus-file=https%3A%2F%2Fscrumorg-website-prod.s3.amazonaws.com%2Fdrupal%2F2019-09%2F2019-09-Kanban-Guide-for-Scrum-Teams-English.pdf
+
+    https: //docs.google.com/presentation/d/1YLla1fSgT7i613xI4SlI9w4xSaFCu2KYZMuWqQXKv9I/edit#slide=id.g75f675e4d0_8_26
