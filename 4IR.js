af66470b0b4abc22dcfd9970934d22eadca6f298
aamirali <sufaamir@gmail.com>
2020-01-05 14:52:53 -0500

    Update files

--- 4IR.js
+++ 4IR.js
@@ -9,4 +9,7 @@ for Agile Teams: https: //www.atlassian.com/software/jira
     https: //www.scrum.org/resources/blog/five-questions-sprint-goal
     https: //scrumorg-website-prod.s3.amazonaws.com/drupal/2019-09/2019-09-Kanban-Guide-for-Scrum-Teams-English.pdf?nexus-file=https%3A%2F%2Fscrumorg-website-prod.s3.amazonaws.com%2Fdrupal%2F2019-09%2F2019-09-Kanban-Guide-for-Scrum-Teams-English.pdf
 
+    https: //docs.google.com/presentation/d/14pnzdfCSKYpqy4CuesBuNHa0-qRLbPmZqsj9brbGp14/edit#slide=id.gc6f9e470d_0_126
     https: //docs.google.com/presentation/d/1YLla1fSgT7i613xI4SlI9w4xSaFCu2KYZMuWqQXKv9I/edit#slide=id.g75f675e4d0_8_26
+
+    https: //edward-designer.com/web/free-pmi-acp-exam-questions/
